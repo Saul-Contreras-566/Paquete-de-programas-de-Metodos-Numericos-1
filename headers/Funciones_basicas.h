63dbf87e2de0ff826a31dbeaf69ded0fdf4744b6
Saúl Contreras <320153398@pcpuma.acatlan.unam.mx>
2025-09-28 13:07:18 +0000

    Update Funciones_basicas.h

--- headers/Funciones_basicas.h
+++ headers/Funciones_basicas.h
@@ -1,10 +1,30 @@
-// Funciones básicas de lectura
+/*
+Funciones básicas que ocuparemos principalmente para la lectura de datos introducidos por el
+usuario.
+*/
 
 #ifndef FUNCIONES_BASICAS_H
-#def FUNCIONES_BASICAS_H
-	void Leer_cadena (char **);
-	char Leer_caracter ();
-	int Leer_entero ();
-	double Leer_real ();
-	int Pregunta_cerrada (char *);
+#define FUNCIONES_BASICAS_H
+        // Constantes
+        #define PI 3.141592653589793
+        #define EULER 2.718281828459045
+
+        // Funciones de lectura de texto
+        void Leer_cadena (char **cadena);
+        char Leer_caracter (); 
+
+        // Funciones de lectura de números enteros
+        int Leer_entero (); 
+        int Leer_entero_entre (int minimo, int maximo);
+        int Leer_entero_que_sea (char *comparador, int numero_criterio);
+
+        // Funciones de lectura de números reales
+        double Leer_real (); 
+        double Leer_real_entre (double minimo, double maximo);
+        double Leer_real_que_sea (char *comparador, double numero_criterio);
+
+        // Otras funciones
+        int Pregunta_cerrada (char *mensaje);
+        void LIMPIAR_PANTALLA (); 
+        void ESPERAR_TECLA (); 
 #endif
