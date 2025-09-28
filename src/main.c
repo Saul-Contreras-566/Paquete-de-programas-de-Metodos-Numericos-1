640fa7bdbfeb6217f8c000d1957287cfc199c081
Saúl Contreras <320153398@pcpuma.acatlan.unam.mx>
2025-09-28 18:17:37 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "Funciones_basicas.h"
 #include "Opcion_01.h"
 
@@ -6,6 +7,10 @@
 
 int main () {
 	int seleccion;
+
+	#ifdef _WIN32
+    system ("chcp 65001"); // Permitiendo acentos en Windows
+    #endif
 
 	// Portada del programa
 	LIMPIAR_PANTALLA ();
